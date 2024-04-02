e5e30d38522250ba4cade3ad67fc77639f080471
igorjssouza <igorjs.souza@gmail.com>
2024-04-02 16:22:46 -0300

    scrip to read a fortran binary library

---
+++ read_fortran_lib.py
@@ -0,0 +1,6 @@
+import fibonacci_factorial_lib as ffb
+
+n1=5
+n2=10
+print(f'the factorial of {n1}th is {ffb.math_functions.factorial(n1)}')
+print(f'The fibonacci sequence of {n2} numbers is {ffb.math_functions.fibonacci(n2)}')
